93f1dbf062d64f849689a945c69852e70560538a
DaigoPaez <126517336+DaigoPaez@users.noreply.github.com>
2023-05-02 19:51:26 +0000

    Avance U5 practica try catch

---
+++ Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+    {
+    static void Main()
+    {
+        uint? edad = null;
+        Console.WriteLine("Captura tu edad y presiona 'Enter'");
+        string? edadCapturada = Console.ReadLine();
+        if (edadCapturada is not null)
+        {
+            try
+            {
+                edad = uint.Parse(edadCapturada);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Introduce un número bobolon");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("No mientas");
+            }
+        }
+        if (edad is not null)
+        {
+            Console.WriteLine($"Tu edad es {edad}");
+        }
+        else
+        {
+            Console.WriteLine("Por favor");
+        }
+    }
+}
