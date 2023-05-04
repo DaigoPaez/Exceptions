5731164ad8b98a8be570f8a0414d46bf13d039f6
DaigoPaez <126517336+DaigoPaez@users.noreply.github.com>
2023-05-04 19:57:15 +0000

    euuueuu

---
+++ Exceptions-04-05-2023/Program.cs
@@ -0,0 +1,20 @@
+﻿class MiException : Exception 
+{
+    public MiException(string message) : base(message)
+    {}
+    public MiException() : this("Error")
+    {}
+}
+
+Internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            throw new MiException("Error");
+        }
+        catch (MiException e) 
+        { }
+    }
+}
